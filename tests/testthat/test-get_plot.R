cfd301cb7c8901cb8419e176915bc6e9cfe93ac8
Jim Regetz <regetz@nceas.ucsb.edu>
2025-02-08 08:18:10 -0800

    Add mocked tests of get_plot using httptest2

--- tests/testthat/test-get_plot.R
+++ tests/testthat/test-get_plot.R
@@ -1,3 +1,22 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+with_mock_api({
+  test_that("get_plot() works", {
+    old_base <- get_vb_base_url()
+    set_vb_base_url(NULL)
+
+    expect_GET(
+      get_plot("some_accession_code"),
+      "https://api.vegbank.org/plot/some_accession_code"
+    )
+
+    response <- get_plot("VB.PL.48373.VZ17QEZ6PVLCDPY")
+    expect_s3_class(response, "data.frame")
+    expect_equal(nrow(response), 1)
+    expect_named(response, c("accessioncode", "latitude",
+      "longitude", "plot_id"), ignore.order=TRUE)
+
+    set_vb_base_url(old_base)
+  })
+})
