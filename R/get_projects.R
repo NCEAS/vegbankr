18d3cfda370a207be472beb29b2bba8f5a1ba69d
Jim Regetz <regetz@nceas.ucsb.edu>
2025-05-29 12:13:02 -0700

    Add functions to get one/all projects

---
+++ R/get_projects.R
@@ -0,0 +1,36 @@
+#' Get a single project
+#'
+#' Return a single project record from VegBank, using its accession
+#' code.
+#'
+#' @param accession_code A project accession code
+#' @return A data frame (one row), or an empty data frame if there is
+#' no matching record for the provided accession code
+#' @examples \dontrun{
+#' get_project("VB.Pj.340.ACADIANATIONALP")
+#' }
+#' @import httr2
+#' @export
+get_project <- function(accession_code) {
+  resource <- "projects"
+  get_resource_by_code(resource, accession_code)
+}
+
+#' Get all projects
+#'
+#' Return a paginated set of project records from VegBank.
+#'
+#' @return A data frame
+#' @param limit Number of records to return
+#' @param offset Number of records to skip
+#' @param detail Desired level of detail ("minimal", "full")
+#' @examples \dontrun{
+#' get_all_projects()
+#' }
+#' @import httr2
+#' @export
+get_all_projects <- function(limit=100, offset=0,
+    detail = c("minimal", "full")) {
+  resource <- "projects"
+  get_all_resources(resource, limit, offset, detail)
+}
