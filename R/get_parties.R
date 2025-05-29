d9fb68fcc39245f695f974a3acf9ad38839f91ce
Jim Regetz <regetz@nceas.ucsb.edu>
2025-05-29 11:53:42 -0700

    Add functions to get one/all parties

---
+++ R/get_parties.R
@@ -0,0 +1,35 @@
+#' Get a single party
+#'
+#' Return a single party record from VegBank, using its accession code.
+#'
+#' @param accession_code A party accession code
+#' @return A data frame (one row), or an empty data frame if there is
+#' no matching record for the provided accession code
+#' @examples \dontrun{
+#' get_party("VB.py.191378.VOLUNTEER")
+#' }
+#' @import httr2
+#' @export
+get_party <- function(accession_code) {
+  resource <- "parties"
+  get_resource_by_code(resource, accession_code)
+}
+
+#' Get all parties
+#'
+#' Return a paginated set of party records from VegBank.
+#'
+#' @return A data frame
+#' @param limit Number of records to return
+#' @param offset Number of records to skip
+#' @param detail Desired level of detail ("minimal", "full")
+#' @examples \dontrun{
+#' get_all_parties()
+#' }
+#' @import httr2
+#' @export
+get_all_parties <- function(limit=100, offset=0,
+    detail = c("minimal", "full")) {
+  resource <- "parties"
+  get_all_resources(resource, limit, offset, detail)
+}
---
+++ tests/testthat/test-get_party.R
@@ -0,0 +1,30 @@
+with_mock_api({
+  test_that("get_party() works", {
+    local_base_url(NULL)
+
+    expect_GET(
+      get_party("some_accession_code"),
+      "https://api.vegbank.org/parties/some_accession_code"
+    )
+
+    response <- get_party("VB.py.191378.VOLUNTEER")
+    expect_s3_class(response, "data.frame")
+    expect_identical(nrow(response), 1L)
+    expect_named(
+      response,
+      c("accession_code", "contact_instructions", "current_name_id",
+        "d_obs_count", "email", "given_name", "middle_name",
+        "organization_name", "party_id", "party_public", "party_type",
+        "salutation", "surname"),
+      ignore.order = TRUE
+    )
+    expect_identical(response$accession_code,
+                     "VB.py.191378.VOLUNTEER")
+    expect_identical(response$d_obs_count,
+                     1123L)
+    expect_identical(response$party_public,
+                     TRUE)
+    expect_identical(response$contact_instructions,
+                     NA)
+  })
+})
