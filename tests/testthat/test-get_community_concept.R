d6e36365405deeb31be1df63d31b004c6b81df37
Jim Regetz <regetz@nceas.ucsb.edu>
2025-05-29 11:12:00 -0700

    Add functions to get one/all community concepts
    This also adds one new field to the canonical names lookup table, and
    cleans up a duplicate entry.

---
+++ tests/testthat/test-get_community_concept.R
@@ -0,0 +1,29 @@
+with_mock_api({
+  test_that("get_community_concept() works", {
+    local_base_url(NULL)
+
+    expect_GET(
+      get_community_concept("some_accession_code"),
+      "https://api.vegbank.org/community-concepts/some_accession_code"
+    )
+
+    response <- get_community_concept("VB.cc.30617.ARTEMISIATRIDEN")
+    expect_s3_class(response, "data.frame")
+    expect_identical(nrow(response), 1L)
+    expect_named(
+      response,
+      c("accession_code", "class_system", "comm_description",
+        "comm_name", "comm_name_date_entered", "comm_name_status",
+        "current_accepted", "default_name", "obs_count",
+        "party_accession_code", "party_id", "reference_id",
+        "usage_start", "usage_stop"),
+      ignore.order = TRUE
+    )
+    expect_identical(response$accession_code,
+                     "VB.cc.30617.ARTEMISIATRIDEN")
+    expect_identical(response$obs_count,
+                     336L)
+    expect_identical(response$comm_description,
+                     NA)
+  })
+})
