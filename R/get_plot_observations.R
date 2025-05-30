3ad1ceb9ea7ceac6f44c14d48ce2f060b4e16b86
Jim Regetz <regetz@nceas.ucsb.edu>
2025-05-29 21:31:27 -0700

    Add wrapper for get_observation_details endpoint
    Note that this API call returns a more complicated JSON response than
    the other main GET resource endpoints. If the provided accession code
    matches a plot observation in the DB, the response is a JSON list that
    contains one element. This element has keys "taxa" and "communities"
    that themselves each correspond to a list of table records (for taxon
    observations and community classifications, respectively), while all
    other keys have a single atomic value and collectively correspond to
    fields of the plot observation record itself. The R wrapper function
    handles all of this, returning a list of three dataframes
    (plot_observation, taxa, and communities).

--- R/get_plot_observations.R
+++ R/get_plot_observations.R
@@ -34,3 +34,42 @@ get_all_plot_observations <- function(limit=100, offset=0,
   resource <- "plot-observations"
   get_all_resources(resource, limit, offset, detail)
 }
+
+#' Get details for a plot observation
+#'
+#' Return details about a plot observation record from VegBank,
+#' including plot observation details, taxon observation, and community
+#' classifications, and more, using the plot observation accession code.
+#'
+#' @param accession_code A plot observation accession code
+#' @return A list of 3 data frames, or an empty list if there is no
+#' matching record for the provided accession code
+#' @examples \dontrun{
+#' get_plot_observation_details("VB.Ob.2948.ACAD143")
+#' }
+#' @import httr2
+#' @export
+get_plot_observation_details <- function(accession_code) {
+  resource <- "get_observation_details"
+  sub_table_names <- c("taxa", "communities")
+  request <- request(get_vb_base_url()) |>
+    req_url_path_append(resource) |>
+    req_url_path_append(accession_code) |>
+    req_headers(Accept = "application/json")
+  response <- req_perform(request)
+  response_json <- resp_body_json(response)[[1]]
+  if (!"error" %in% names(response_json)) {
+    df_list <- sapply(sub_table_names, function(table_name) {
+        jsonlist2df(response_json[[table_name]])
+      }, simplify=FALSE)
+    obs_data_jsonlist <- response_json[!names(response_json) %in%
+                                       sub_table_names]
+    obs_df <- jsonlist2df(list(obs_data_jsonlist))
+    df_list <- append(df_list, list(plot_observation=obs_df), after = 0)
+    df_list <- sapply(df_list, canonicalize_names)
+    return(df_list)
+  } else {
+    warning(response_json[["error"]], call. = FALSE)
+    invisible(NULL)
+  }
+}
---
+++ tests/testthat/test-get_observation_details.R
@@ -0,0 +1,79 @@
+with_mock_api({
+  test_that("get_plot_observation_details() works", {
+    local_base_url(NULL)
+
+    expect_GET(
+      get_plot_observation_details("some_accession_code"),
+      "https://api.vegbank.org/get_observation_details/some_accession_code"
+    )
+
+    expect_warning(
+       error_response <- get_plot_observation_details("unknown_accession_code"),
+       "No observation found with that accession code.",
+       fixed = TRUE
+    )
+    expect_null(error_response)
+
+    response <- get_plot_observation_details("VB.Ob.41618.50D47AJX5G5U8WY")
+    expect_type(response, "list")
+    expect_identical(length(response), 3L)
+    expect_named(
+      response,
+      c("plot_observation", "taxa", "communities"),
+      ignore.order = TRUE
+    )
+    df_plot_observation <- response[["plot_observation"]]
+    expect_s3_class(df_plot_observation, "data.frame")
+    expect_identical(nrow(df_plot_observation), 1L)
+    expect_named(
+      df_plot_observation,
+      c("area", "author_obs_code", "author_plot_code",
+        "auto_taxon_cover", "bryophyte_quality", "confidentiality_text",
+        "country", "cover_method_accession_code", "cover_type",
+        "date_entered", "effort_level", "elevation",
+        "floristic_quality", "interp_current_partyname", "latitude",
+        "lichen_quality", "location_narrative", "longitude",
+        "obs_accession_code", "obs_end_date", "obs_start_date",
+        "permanence", "plot_accession_code", "plot_validation_level",
+        "plot_validation_level_descr", "project_accession_code",
+        "project_name", "slope_aspect", "slope_gradient",
+        "state_province", "stratum_method_accession_code",
+        "stratum_method_description", "stratum_method_name",
+        "taxon_observation_area"),
+      ignore.order = TRUE
+    )
+    expect_identical(df_plot_observation$longitude,
+                      -106.410502828997)
+    expect_identical(df_plot_observation$lichen_quality,
+                      NA)
+    expect_identical(df_plot_observation$plot_validation_level,
+                      1L)
+
+    df_taxa <- response[["taxa"]]
+    expect_s3_class(df_taxa, "data.frame")
+    expect_identical(nrow(df_taxa), 8L)
+    expect_named(
+      df_taxa,
+      c("author_plant_name", "basal_area", "biomass", "cover",
+        "cover_code", "inference_area", "int_curr_plant_code",
+        "int_curr_plant_common", "int_curr_plant_sci_full",
+        "int_curr_plant_sci_name_no_auth", "int_orig_plant_code",
+        "int_orig_plant_common", "int_orig_plant_sci_full",
+        "int_orig_plant_sci_name_no_auth", "stratum"),
+      ignore.order = TRUE
+    )
+    expect_identical(df_taxa$int_curr_plant_sci_full[8],
+                     "Lupinus L.")
+
+    df_communities <- response[["communities"]]
+    expect_s3_class(df_communities, "data.frame")
+    expect_identical(nrow(df_communities), 1L)
+    expect_named(
+      df_communities,
+      c("accession_code", "comm_name"),
+      ignore.order = TRUE
+    )
+    expect_identical(df_communities$accession_code,
+                     "VB.cc.30617.ARTEMISIATRIDEN")
+  })
+})
