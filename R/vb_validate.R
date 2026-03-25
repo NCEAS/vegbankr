949abc2fdf6fae088403e0c9936cea861afe99fd
Jeanette Clark <jclark@nceas.ucsb.edu>
2026-03-25 11:43:35 -0700

    make optional fk validators quieter

--- R/vb_validate.R
+++ R/vb_validate.R
@@ -501,22 +501,31 @@ validate_values_exist <- function(child_df, child_col, parent_df, parent_col, op
   parent_table <- deparse(substitute(parent_df))
   
   if (is.null(child_df) || is.null(parent_df)) {
-    cli::cli_alert_info("{child_table} or {parent_table} not provided - skipping foreign key validation.")
-    return(TRUE)
+    if (optional){
+      return(TRUE)
+    } else {
+      cli::cli_alert_info("{child_table} or {parent_table} not provided - skipping foreign key validation.")
+      return(TRUE) 
+    }
   }
   
   if (!(child_col %in% names(child_df))) {
-    cli::cli_alert_info("{child_table}: Column '{child_col}' not found - skipping foreign key validation.")
     if (optional) {
       return(TRUE)
-    } else return(FALSE)
+    } else {
+      cli::cli_alert_info("{child_table}: Column '{child_col}' not found - skipping foreign key validation.")
+      return(FALSE)
+    }
   }
   
   if (!(parent_col %in% names(parent_df))) {
-    cli::cli_alert_info("{child_table}: Column '{parent_col}' not found in {parent_table} - skipping foreign key validation")
+    
     if (optional) {
       return(TRUE)
-    } else return(FALSE)
+    } else{
+      cli::cli_alert_info("{child_table}: Column '{parent_col}' not found in {parent_table} - skipping foreign key validation")
+      return(FALSE)    
+    } 
   }
   
   orphaned <- child_df %>%
